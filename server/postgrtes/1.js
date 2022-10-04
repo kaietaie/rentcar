7054445bebd21e908bd6144b8f0c268dce69d1f9
kaieta <kaieta.ievgenii@gmail.com>
2022-10-04 12:18:28 +0300

    Make new DB in PostgreSQL, update tasks

--- server/postgrtes/1.js
+++ server/postgrtes/1.js
@@ -1,23 +1,21 @@
-'use strict';
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: '127.0.0.1',
+  host: 'localhost',
   port: 5432,
   database: 'carrentdb',
   user: 'kaieta',
   password: 'kaieta',
 });
 
-const fields = ['schemaname', 'tablename', 'tableowner'].join(', ');
-const sql = `SELECT ${fields} FROM pg_tables WHERE tableowner = $1`;
-pool.query(sql, ['kaieta'], (err, res) => {
+// const sql = `INSERT INTO cars VALUES (1,'Superb', 'Skoda', '2.0 TDI, 115kW', 6.2, 625, 5)`;
+const sql = `SELECT * FROM Cars`
+pool.query(sql, (err, res) => {
   if (err) {
     throw err;
   }
-  console.dir({ res });
-  console.table(res.fields);
+  // console.dir({ res });
+  // console.table(res.fields);
   console.table(res.rows);
   pool.end();
 });
--- server/server.mjs
+++ server/server.mjs
@@ -2,26 +2,27 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import { getCars, postCars } from "./routes/cars.mjs";
-
+// import { getCars, postCars } from "./routes/cars.mjs";
+import { carsrouter } from "./routes/cars.routes.mjs";
 
 const port = 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', carsrouter)
 
-const uri = process.env.EXPRESS_NODE_ATLAS_URI;
-mongoose.connect(uri);
+// const uri = process.env.EXPRESS_NODE_ATLAS_URI;
+// mongoose.connect(uri);
 
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("mongoDB connection established successfully");
-});
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("mongoDB connection established successfully");
+// });
 
-app.get("/cars", getCars);
+// app.get("/cars", getCars);
 
-app.post("/cars", postCars);
+// app.post("/cars", postCars);
 
 app.listen(port, () => {
   console.log(`Server is working on localhost:${port}`);
