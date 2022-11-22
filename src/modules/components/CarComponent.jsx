666adc488d38b46c1c6ba5ba6947ba8aaf673a4e
Ievgenii Kaieta <kaieta.ievgenii@gmail.com>
2022-11-23 00:31:38 +0200

    Some changes, delete comments

--- src/modules/components/CarComponent.jsx
+++ src/modules/components/CarComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import CarsDB from "./cars";
+import CarsDB from "./CarsAPI";
 
 export default function Car(props) {
   const findCar = props.car.name;
@@ -8,20 +8,21 @@ export default function Car(props) {
   useEffect(() => {
     const fetchCars = async () => {
       const result = await CarsDB(findCar);
-      setActualCar(result);
+      return setActualCar(result[0]);
     };
     fetchCars();
   }, [findCar]);
+  const clima =  actualCar.clima == 1 ? "Є кондиціонер" : "Без кондиціонеру";
   if (!Object.keys(actualCar).length) {
     return <>loading...</>;
   } else {
     return (
       <>
-        Мест {actualCar.properties.passengers} <br />
-        Кондиционер <br />
-        Объем багажника {actualCar.properties.trunk} л.
+        Кількість місць {actualCar.seats} <br />
+        { clima }<br />
+        Об'ієм багажника {actualCar.trunk} л.
         <br />
-        Расход {actualCar.properties.consumption} л./100 км
+        Витрата палива {actualCar.consumption} л./100 км
         <br />
       </>
     );
