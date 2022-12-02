554af875bf7e07eaef88f932db59f2a8b565a868
tis86 <emporio86@gmail.com>
2022-12-02 21:22:28 +0200

    update hostname

--- server/server.mjs
+++ server/server.mjs
@@ -5,11 +5,12 @@ import { carsrouter } from "./routes/cars.routes.mjs";
 
 const port = 5000;
 const app = express();
+const hostname = os.hostname();
 
 app.use(cors());
 app.use(express.json());
 app.use('/apicars', carsrouter)
 
-app.listen(port, () => {
-  console.log(`Server is working on localhost:${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server is working on ${hostname}:${port}`);
 });
