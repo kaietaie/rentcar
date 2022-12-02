da0fc8bb826433d7587b03728495839356ecc18b
tis86 <emporio86@gmail.com>
2022-12-02 19:53:32 +0200

    resolve merge conflict

--- src/modules/components/LoginHeader.jsx
+++ src/modules/components/LoginHeader.jsx
@@ -3,16 +3,8 @@ import LoginHeaderComponent from "./LoginHeaderComponent";
 import LogoutHeaderComponent from "./LogoutHeaderComponent";
 import { useAuth } from "../context/AuthContext";
 
-<<<<<<< HEAD
-export default function LoginHeader() {
-  const currentUser = useAuth();
-  if (!currentUser.currentUser?.email) {
-      return <LogoutHeaderComponent />
-    } 
-=======
 const LoginHeader = ({ isMobile }) => {
     const currentUser = useAuth();
->>>>>>> CAR-20-adaptive-header-and-main-page
 
     return (
         <div className={`${!isMobile ? 'login': ""}`}>
