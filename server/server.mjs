0347e0a2bbccbb1964c43bcc56c83a272402989f
tis86 <emporio86@gmail.com>
2022-12-02 21:18:49 +0200

    Revert "changing localhost to hostname"
    This reverts commit 82dd0ee13cfe146a5704e3e193c2e4b30b760c42.

--- server/server.mjs
+++ server/server.mjs
@@ -2,16 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { carsrouter } from "./routes/cars.routes.mjs";
-import os from 'os';
 
 const port = 5000;
 const app = express();
-const hostname = os.hostname();
 
 app.use(cors());
 app.use(express.json());
 app.use('/apicars', carsrouter)
 
-app.listen(port, hostname, () => {
-  console.log(`Server is working on ${hostname}:${port}`);
+app.listen(port, () => {
+  console.log(`Server is working on localhost:${port}`);
 });
