4e3ad664e98d9ea5b4d6f678232349995980982b
tis86 <emporio86@gmail.com>
2022-12-14 23:37:44 +0200

    update dbConnectComponent.mjs; from process.env.HOST to process.env.POSTGRES_HOST; bad naming;

--- server/dbConnectComponent.mjs
+++ server/dbConnectComponent.mjs
@@ -2,7 +2,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 export const pool = new Pool({
-    host: process.env.HOST,
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
