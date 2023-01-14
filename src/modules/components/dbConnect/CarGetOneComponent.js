795573bf9f1099cd453547f9bd6d8ab8ad912cff
kaieta <kaieta.ievgenii@gmail.com>
2023-01-14 22:36:16 +0200

    q

--- src/modules/components/dbConnect/CarGetOneComponent.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default async function CarGetOne(car) {
-  const { data } = await axios.get("http://localhost:5000/api/cars");
-  return data.find((r) => r.model === car);
-}
--- src/modules/components/dbConnect/CreateCarComponent.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default async function createCar(car) {
-  await axios
-    .post("http://localhost:5000/api/cars", car)
-    .catch(function (error) {
-      console.log(error);
-    });
-}
