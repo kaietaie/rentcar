c2f7c3047b00838fa16e0d8225ed6e913afb2c9f
Ievgenii Kaieta <kaieta.ievgenii@gmail.com>
2023-02-22 15:28:01 +0200

    Fix delete user function

--- server/dbUsersControllers/deleteUserComponent.mjs
+++ server/dbUsersControllers/deleteUserComponent.mjs
@@ -5,6 +5,7 @@ export default async function deleteUser(req, res) {
   try {
     const { user_email } = req.body;
     const user = await getUser({"user_email":user_email});
+    console.log(user)
     if (!user) {
       return res
         .status(400)
@@ -13,7 +14,7 @@ export default async function deleteUser(req, res) {
 
     const sql = "UPDATE Users SET enabled=false WHERE user_email= $1;";
     const deleteUser = await pool.query(sql, [user_email]);
-    if (deleteUser.command === "DELETE") res.json("User was deleted");
+    if (deleteUser.command === "UPDATE") res.json("User was deleted");
   } catch (error) {
     console.log(error);
     res.json({ message: "Something goes wrong!" });
